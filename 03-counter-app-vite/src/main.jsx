b57a65334e68b6d632e8f309c32ce65d2dcaeadf
Tolosacoswal <qs.wal123@gmail.com>
2022-09-14 08:51:23 -0500

    correccion 14-09-22

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -10,11 +10,11 @@ import "./estilos.css";
 ReactDOM.createRoot(document.getElementById ("root")).render(
     <React.StrictMode>
         
-<<<<<<< HEAD
-        <CounterApp value={55}  />
-=======
+
+       
+
         <CounterApp value={20}  />
->>>>>>> 702a211a19a1bcdc5cae27ec8e49ce357a8c47b8
+
         
     </React.StrictMode>
 )
